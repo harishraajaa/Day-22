80ff9e6bff1e7ec87f78607bcff7db3ac488bd35
harishraajaa <rvharishraajaa@gmail.com>
2024-07-09 11:47:19 +0530

    Added Imports

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import Icongrid from './Components/Icongrid'
 import Imageshowcase from './Components/Imageshowcase'
 import Testimonials from './Components/Testimonials'
 import Footer from './Components/Footer'
+import TestImage1 from '../src/assets/img/testimonials-1.jpg'
+import TestImage2 from '../src/assets/img/testimonials-2.jpg'
+import TestImage3 from '../src/assets/img/testimonials-3.jpg'
+import ImageShow1 from '../src/assets/img/bg-showcase-1.jpg'
+import ImageShow2 from '../src/assets/img/bg-showcase-2.jpg'
+import ImageShow3 from '../src/assets/img/bg-showcase-3.jpg'
 
 function App() {
 //Icon Grid component input data
@@ -32,21 +38,21 @@ function App() {
       showtitle:"Fully Responsive Design",
       class1:"col-lg-6 order-lg-2 text-white showcase-img",
       class2:"col-lg-6 order-lg-1 my-auto showcase-text",
-      style:{"backgroundImage": "url('src/assets/img/bg-showcase-1.jpg')"},
+      style:{"backgroundImage": `url(${ImageShow1})`},
       showdesc:"When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!"
     },
     {
       showtitle:"Updated For Bootstrap 5",
       class1:"col-lg-6 my-auto showcase-img",
       class2:"col-lg-6 my-auto showcase-text",
-      style:{"backgroundImage": "url('src/assets/img/bg-showcase-2.jpg')"},
+      style:{"backgroundImage": `url(${ImageShow2})`},
       showdesc:"Newly improved, and full of great utility classes, Bootstrap 5 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 5!"
     },
     {
       showtitle:"Easy to Use & Customize",
       class1:"col-lg-6 order-lg-2 text-white showcase-img",
       class2:"col-lg-6 order-lg-1 my-auto showcase-text",
-      style:{"backgroundImage": "url('src/assets/img/bg-showcase-3.jpg')"},
+      style:{"backgroundImage": `url(${ImageShow3})`},
       showdesc:"Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!"
     }
   ]
@@ -54,17 +60,17 @@ function App() {
 //Testimonial Input Data
   let testdata=[
     { 
-      timage:"/src/assets/img/testimonials-1.jpg",
+      timage:TestImage1,
       tname:"Margaret E.",
       tdesc:"This is fantastic! Thanks so much guys!"
     },
     {
-      timage:"/src/assets/img/testimonials-2.jpg",
+      timage:TestImage2,
       tname:"Fred S.",
       tdesc:"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."
     },
     {
-      timage:"/src/assets/img/testimonials-3.jpg",
+      timage:TestImage3,
       tname:"Sarah W.",
       tdesc:"Thanks so much for making these free resources available to us!"
     }
@@ -109,10 +115,10 @@ function App() {
     </div>
   </section> 
   {/* <!-- Testimonials--> */}
-  <section class="testimonials text-center bg-light">
-    <div class="container">
-        <h2 class="mb-5">What people are saying...</h2>
-          <div class="row">
+  <section className="testimonials text-center bg-light">
+    <div className="container">
+        <h2 className="mb-5">What people are saying...</h2>
+          <div className="row">
           {
             testdata.map((e,i)=>{
               return <Testimonials data={e} key={i}/>
@@ -123,11 +129,11 @@ function App() {
       </div>
   </section>
   {/* <!-- Call to Action--> */}
-  <section class="call-to-action text-white text-center" id="signup">
-    <div class="container position-relative">
-      <div class="row justify-content-center">
-        <div class="col-xl-6">
-          <h2 class="mb-4">Ready to get started? Sign up now!</h2>
+  <section className="call-to-action text-white text-center" id="signup">
+    <div className="container position-relative">
+      <div className="row justify-content-center">
+        <div className="col-xl-6">
+          <h2 className="mb-4">Ready to get started? Sign up now!</h2>
           <Formcomp/>
         </div> 
       </div> 
